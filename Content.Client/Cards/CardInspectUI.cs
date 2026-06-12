a98a4fe95c00a845355548748008604df5ba91a7
riccardi48 <richardballard00@gmail.com>
2026-06-12 18:13:35 +0100

    Made it work

--- Content.Client/Cards/CardInspectUI.cs
+++ Content.Client/Cards/CardInspectUI.cs
@@ -1,12 +1,7 @@
-using System.Linq;
 using System.Numerics;
 using Content.Shared.Cards;
-using Content.Shared.Stacks;
-using Robust.Client.Graphics;
 using Robust.Client.UserInterface.Controls;
-using Robust.Shared.Graphics.RSI;
 using Robust.Shared.Utility;
-using static Robust.Client.UserInterface.Control;
 
 namespace Content.Client.Cards;
 
@@ -16,74 +11,15 @@ public sealed partial class CardSystem
     {
         CardInspect menu = new CardInspect { VisualState = GetCardListVisualState(cards) };
 
-        var prototype = Prototype(cards.Owner);
-        if (prototype == null)
-            return;
-
-        var textures = _sprite.GetPrototypeTextures(prototype).ToList();
-
-        var icon = _sprite.GetPrototypeIcon(prototype);
-
-
-        TextureRect texture = new TextureRect
+        var spriteView = new SpriteView
         {
-            Texture = icon.GetFrame(RsiDirection.South, 0),
-            TextureScale = new Vector2(10, 10),
+            OverrideDirection = Direction.South,
+            SetSize = new Vector2(250, 250),
+            Stretch = SpriteView.StretchMode.Fill,
         };
-        menu.Box.AddChild(new EntityPrototypeView(prototype, entMan));
+        spriteView.SetEntity(cards);
+        menu.SpriteHolder.AddChild(spriteView);
 
-        var resourcePath = new ResPath(RsiPath);
-
-        var count = menu.VisualState.CardList.Count;
-
-        for (var i = 0; i < count; i++)
-        {
-            var card = menu.VisualState.CardList[i];
-
-            var (baseLayer, layerOne, layerTwo) = CardLayers(i);
-
-            if (!_prototypeManager.TryIndex<CardPrototype>(card.CardId, out var cardPrototype))
-                continue;
-
-            BuildUICard(cardPrototype, baseLayer, card.BaseState, layerOne, layerTwo, menu, resourcePath);
-        }
         _examineSystem.SendExamineControl(player, cards.Owner, menu, false);
-    }
-
-    public void BuildUICard(
-        CardPrototype prototype,
-        string baseLayer,
-        string baseSprite,
-        string layerOne,
-        string layerTwo,
-        CardInspect menu,
-        ResPath resourcePath
-    )
-    {
-        BuildUILayer(baseLayer, new SpriteSpecifier.Rsi(resourcePath, prototype.LayerBaseState ?? baseSprite), menu);
-        if (prototype.LayerOneState != null)
-            BuildUILayer(layerOne, new SpriteSpecifier.Rsi(resourcePath, prototype.LayerOneState), menu);
-        if (prototype.LayerTwoState != null)
-            BuildUILayer(layerTwo, new SpriteSpecifier.Rsi(resourcePath, prototype.LayerTwoState), menu);
-    }
-
-    public void BuildUILayer(string layer, SpriteSpecifier.Rsi? specifier, CardInspect menu)
-    {
-        if (specifier == null)
-            return;
-        TextureRect texture = new TextureRect
-        {
-            Name = layer,
-            Texture = _sprite.Frame0(specifier),
-            TextureScale = new Vector2(10, 10),
-        };
-        menu.Box.AddChild(texture);
-    }
-
-    public void TransformUILayer(int idx, Vector2 movement, Angle rotation, CardInspect menu)
-    {
-        var texture = menu.Box.GetChild(idx);
-        menu.Box.GetChild(idx)
-            .Arrange(new UIBox2(movement - texture.DesiredSize / 2, movement + texture.DesiredSize / 2));
     }
 }
