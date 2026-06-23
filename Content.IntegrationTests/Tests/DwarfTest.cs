d15ae73e75f98b53c4d598752ee1c2b07577ba71
riccardi48 <richardballard00@gmail.com>
2026-06-23 22:11:48 +0100

    review requests

--- Content.IntegrationTests/Tests/DwarfTest.cs
+++ Content.IntegrationTests/Tests/DwarfTest.cs
@@ -1,24 +1,22 @@
 using Content.IntegrationTests.Fixtures;
+using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Shared.Humanoid.Prototypes;
+using Robust.Shared.Prototypes;
 
 namespace Content.IntegrationTests.Tests;
 
 public sealed class DwarfTest : GameTest
 {
-    private const string Dwarf = "Dwarf";
+    private readonly ProtoId<SpeciesPrototype> _dwarf = "Dwarf";
 
     [Test]
     [Description("Checks that dwarfs are still in the game")]
+    [RunOnSide(Side.Server)]
     public async Task DwarfsNotRemovedTest()
     {
-        await Server.WaitAssertion(() =>
+        if (!SProtoMan.HasIndex(_dwarf))
         {
-            if (!SProtoMan.HasIndex<SpeciesPrototype>(Dwarf))
-            {
-                Assert.Fail(
-                    "DWARFS REMOVED!!! Freak the fuck out and panic sell everything right now. It's fucking over."
-                );
-            }
-        });
+            Assert.Fail("DWARFS REMOVED!!! Freak the fuck out and panic sell everything right now. It's fucking over.");
+        }
     }
 }
